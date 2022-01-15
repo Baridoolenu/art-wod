2ff0dafe46045c42683c975fc0302bf539324b1e
Baridoolenu <58880407+Baridoolenu@users.noreply.github.com>
2022-01-15 22:19:42 +0100

    Add files via upload

---
+++ Art sealing site/index.php
@@ -0,0 +1,134 @@
+<!doctype html>
+<html>
+
+<head>
+<title>Drawing</title>
+<link rel="stylesheet" type="text/css" href="style/design1.css">
+
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=on">
+</head>
+
+<body>
+<section class="one">
+
+    <div>
+<p>logo</p>
+    <nav>
+        
+
+        <a>home</a>
+        <a>dashboard</a>
+        <a>drawing</a>
+        <a>painting</a>
+        <a>pricing</a>
+        <a>about</a>
+        <a>shop</a>
+        <a>login</a>
+        <a>basket_icon</a>
+        <a>search_icon</a>
+
+</nav>
+
+</div>
+
+
+        <div>
+                <div>
+                            <h3>YOUR FLEXIBLE ONLINE ART SCHOOL</h3>
+
+                            <p>Art-Wod provides you with a well sturctured template to learn all fundamentals. From drawing to painting, you will recieve weekly new content that is designed to help you learn efficiently!</p>
+
+                            <input> Become an Artlete</input>
+                            <input> Discover the programs </input>
+                </div>
+                <div>
+                    <p>logo fully defined</p>
+                    <p>Your weekly dose of progress</p>
+                </div>
+</div>
+</section>
+
+
+<section class="two">
+
+<div>
+    <h3>COMMUNITY BASED COURSES<h3>
+        <p>Art-wod's biggest strength is it's community.hundreds of artists from all skill levels and walks of life coming together to tackle the same assignments.</p>
+        <p>A unique opportunity for you to learn and interact with your peers, see how they handle the assignments and progress together! </p>
+
+        <input > join the Art_Wod community</input>
+</div>
+<div>
+    <p>the chord_icon</p>
+</div>
+</section>
+
+<section class="three">
+
+
+<div>
+    <p>the girl_with_the_frog_icon</p>
+</div>
+<div>
+    <h3>COMMUNITY BASED COURSES<h3>
+        <h3> TRY FOR FREE</h3>
+
+        <p>we are confident in the effectiveness of our program. Therefore we invite you to try the Drawing prgram yourself. free of charge for 7 days!</p>
+        <p> 62% of people that used our free trial. stayed with the program!</p>
+
+        <input > join the Art_Wod community</input>
+</div>
+</section>
+<section class="four">
+    <div>
+        <h3> NO MORE GUESSING</h3>
+
+        <p> Art-Wod engages you to draw in a well structured manner that is designed to help you progress efficiently.</p>
+        <p> We help you unleash your creative potential whilist you don't have to worry about what to do next.</p>
+        <p> let us worry. you create!</p>
+
+</div>
+
+<p> a man with a bike and a gun</p>
+
+</section>
+
+<section class= "five">
+
+<p> a wizard with a bird</p>
+
+<div>
+<h3>EASY TO FOLLOW</h3>
+
+<p> The Drawing program consists of a two week cycle covering all subject matter, with assignments tailored to all levels of artists. Assignments are easy to follow and accompanied by an instructional video for your convenience!</p>
+<p> The Painting program offers the same setup and is tailored to supplement the more advanced students of the drawing program.</p>
+
+<input> Discover the drawing program</input>
+<input> Discover the painting program</input>
+</div>
+
+</section>
+
+<section class =" full spam field">
+<h3> JOIN THE ART-WOD COMMUINITY</h3>
+<p>Art-wod has been design to be accessible for everyone. Both in subject matter and in pricing!</p>
+<input>sign up today</input>
+
+</section>
+
+<section class="six">
+    <div>
+    <h3> NEW </h3>
+
+    <p> classroom sessions are now a permanent part of the program!</p>
+    <p> During these record live-streams Antonio discusses a lot of foundamental principles and insights that will help you tackle the assignment better!</p>
+<input>Discover classroom sessions</input>
+</div>
+
+<p> vedio of the above</p>
+
+
+
+</body>
+
+</html>
